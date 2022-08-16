2889d95718cc2e11acf19a714c1232165983a710
oskar <os.schtndr@tuta.io>
2022-08-16 17:33:36 +0200

    Added display update with Bit
    Former-commit-id: 1fc765a65bb58f866e9f3d53c8f1a979c082b7f5

--- Scripts/Helpers.js
+++ Scripts/Helpers.js
@@ -6,6 +6,33 @@ function updateParameterWithPercent(parameter, value) {
 	updateDisplay();
 }
 
+function updateParameterWithBit(parameter, value) {
+	var index = Math.round(value * 10) - 4;
+	var bitMapping = [
+		8,
+		10,
+		11,
+		12,
+		13,
+		14,
+		16,
+	];
+
+	STATE.parameter = parameter;
+	STATE.value = bitMapping[index] + ' BIT';
+	
+	updateDisplay();
+}
+
+function updateParameterWithRate(parameter, value) {
+	
+
+	STATE.parameter = parameter;
+	STATE.value = Math.round(value * 10) + ' Hz';
+	
+	updateDisplay();
+}
+
 function updateFreezeParameter(value) {
 	if (value == 1.0) {
 		STATE.freezeMode = true;
