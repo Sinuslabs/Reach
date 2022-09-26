6cfa67f8042c087f1a8e73fa8687fcb55d5a118d
oskar <36965591+oskarsh@users.noreply.github.com>
2022-09-26 13:21:47 +0200

    updated vumeter code fx compatible, mix goes to +10, no noisegen instead gain reduction
    Former-commit-id: 22804ad97d05f55e5769a3ac7e36b92561cb36bf

--- Scripts/VuMeter.js
+++ Scripts/VuMeter.js
@@ -1,16 +1,17 @@
 var vuMeterTimer = Engine.createTimerObject();
 const var header_vumeter_ch1 = Content.getComponent("header_vumeter_ch1");
 const var header_vumeter_ch2 = Content.getComponent("header_vumeter_ch2");
+const var Analyser1 = Synth.getEffect("Analyser1");
 
 vuMeterTimer.setTimerCallback(function()
 {
-	var ch1 = getNormalizedPeakValue(Engine.getMasterPeakLevel(1));
-	var ch2 = getNormalizedPeakValue(Engine.getMasterPeakLevel(0));
+	var ch1 = getNormalizedPeakValue(Analyser1.getCurrentLevel(true));
+	var ch2 = getNormalizedPeakValue(Analyser1.getCurrentLevel());
 	
 	header_vumeter_ch1.setValue(ch2);
 	header_vumeter_ch2.setValue(ch1);
 });
-vuMeterTimer.startTimer(50);
+vuMeterTimer.startTimer(30);
 
 function getNormalizedPeakValue(gain) {
 		return 0.01 * (100.0 + Engine.getDecibelsForGainFactor(gain));
