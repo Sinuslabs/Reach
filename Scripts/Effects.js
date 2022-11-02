3a8e3b1dee4eaa496df07eb43fb5e85e0a76f3b7
oskar <36965591+oskarsh@users.noreply.github.com>
2022-11-02 08:55:08 +0100

    Moved Effects to own namespace

---
+++ Scripts/Effects.js
@@ -0,0 +1,34 @@
+namespace Effects {
+	
+	
+	Content.getComponent("knob_degrade_mix").setControlCallback(onknob_degrade_mixControl);
+	Content.getComponent("knob_effects_flair").setControlCallback(onknob_flair_flairControl);
+
+	// DEGRADE
+	inline function onknob_degrade_mixControl(component, value)
+	{
+		//Degrade.setAttribute(Degrade.PostFilt, value);
+		updateParameterWithLabel('DEGRADE', value, '%');
+		showTempScreen('degrade');
+		
+		
+		DegradeAnimationPanel.setValue(component.get("max") + 1 - value * 20);
+		
+		DEGRADE_STATE.corner = 80 + (value * -80);
+		DegradeAnimationPanel.repaint();
+	};
+	
+	// FLAIR
+	inline function onknob_flair_flairControl(component, value)
+	{
+		//Flair.setAttribute(Flair.WetAmount, value);
+		updateParameterWithLabel('FLAIR', value, '%');
+		showTempScreen('flair');
+		
+		local initialPosX = 300;
+		local move = value * 50;
+		
+		Flair1AnimationPanel.set('x', initialPosX + move);
+		Flair2AnimationPanel.set('x', initialPosX - move);
+	};
+}
