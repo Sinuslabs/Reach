149149dbe36101aa69361cfce3009b5c3a93e4cf
oskar <daeh@tuta.io>
2024-04-29 17:57:13 +0700

    optimized upload script

---
+++ Packaging/Linux/test.sh
@@ -0,0 +1,32 @@
+# TESTING USING PLUGINVAL
+
+source ../../info.env
+
+echo Configure pluginval path
+
+exit 1
+
+pluginval=
+project=$NAME
+script_root=$PWD
+project_root=$(cd "$script_root"/../.. && pwd)
+plugin_path_vst3=$project_root/build/Linux/$NAME.vst3
+plugin_path_vst3=$project_root/build/Linux/$NAME.component
+
+# Log directory
+log_dir="logs"
+rm -rf "$log_dir"
+mkdir -p "$log_dir"
+
+# Command to run pluginval
+echo "Starting plugin validation..."
+$pluginval --validate "$plugin_path_vst3" --strictness-level 8 --output-dir "$log_dir" --verbose
+$pluginval --validate "$plugin_path_au" --strictness-level 8 --output-dir "$log_dir" --verbose
+
+# Check for success or failure
+if [ $? -eq 0 ]; then
+    echo "Plugin validation succeeded."
+else
+    echo "Plugin validation failed."
+    exit 1
+fi
--- Packaging/OSX/run.sh
+++ Packaging/OSX/run.sh
@@ -1,4 +1,18 @@
+#!/bin/bash -eu
+
+# Start total execution time measurement with nanosecond precision
+overall_start=$(date +%s.%N)
+
 sh compile.sh && sh test.sh && sh package.sh && sh upload.sh
+
+# End total execution time measurement and print
+# End total execution time measurement
+overall_end=$(date +%s.%N)
+
+# Calculate the duration in seconds with millisecond precision
+duration=$(echo "$overall_end - $overall_start" | bc)
+echo "Total execution time: $duration s."
+
 
 # Notify the user that the package has been built
 osascript -e "display notification \"${NAME} ${VERSION} has been successfully built.\" with title \"Build Complete\" sound name \"Submarine\""
